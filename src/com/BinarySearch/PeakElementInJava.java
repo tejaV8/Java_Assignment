7ad86d14bef8873bd6a86d3a2b49fda64902e891
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-06-14 19:14:47 +0530

    Binary Search

--- src/com/BinarySearch/PeakElementInJava.java
+++ src/com/BinarySearch/PeakElementInJava.java
@@ -1,15 +1,17 @@
 package com.BinarySearch;
 
-public class PeakElementInJava {  public static void main(String[] args) {
+public class PeakElementInJava {
+    public static void main(String[] args) {
 
-}
+    }
+
     public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
 
         while (start < end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] > arr[mid+1]) {
+            if (arr[mid] > arr[mid + 1]) {
                 // you are in dec part of array
                 // this may be the ans, but look at left
                 // this is why end != mid - 1
@@ -27,4 +29,4 @@ public class PeakElementInJava {  public static void main(String[] args) {
         return start; // or return end as both are =
     }
 }
-}
+
