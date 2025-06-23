c37a05b900b30e7aa70a40cd8c21cff54886237a
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-06-23 20:57:37 +0530

    RBS Duplicate

--- src/com/BinarySearch/RotatedBinarySearch.java
+++ src/com/BinarySearch/RotatedBinarySearch.java
@@ -3,14 +3,14 @@ package com.BinarySearch;
 public class RotatedBinarySearch {
     public static void main(String[] args) {
 
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
+        int[] arr = {2, 2, 2, 5, 6, 2};
+        int target = 2;
         System.out.println(search(arr, target));
     }
 
     public static int search(int[] nums, int target) {
 
-        int pivot = pivotElement(nums);
+        int pivot = pivotDuplicateElement(nums);
 
         if (pivot == -1) {
             return binarySearch(nums, target, 0, nums.length - 1);//If pivot is not found that means it is a normal sorted array
@@ -23,7 +23,7 @@ public class RotatedBinarySearch {
         return binarySearch(nums, target, pivot + 1, nums.length - 1);
     }
 
-
+    // In case there are no duplicates
     static int pivotElement(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
@@ -44,6 +44,46 @@ public class RotatedBinarySearch {
         return -1;
     }
 
+// In case if there are any duplicates
+
+    static int pivotDuplicateElement(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 
     static int binarySearch(int[] arr, int target, int start, int end) {
 
