22abb3d6ea05dca2f01c5dcf8d4863e6131ef6f2
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-06-23 21:07:16 +0530

    Rotation Count

---
+++ src/com/BinarySearch/RotationCount.java
@@ -0,0 +1,70 @@
+package com.BinarySearch;
+
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 5, 6, 7, 9, 2, 2, 2};
+        int count = rotationCountDuplicate(arr);
+
+        System.out.println("The rotation count of the array: " + (count + 1));
+    }
+
+    // In case of no duplicate elements
+    static int rotationCount(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            } else if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int rotationCountDuplicate(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
