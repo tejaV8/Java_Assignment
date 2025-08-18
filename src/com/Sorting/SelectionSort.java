5d6b1f49a998df6103d8dbbaf444713bdf6c5c1c
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-08-18 20:05:41 +0530

    Selection Sort

--- src/com/Sorting/SelectionSort.java
+++ src/com/Sorting/SelectionSort.java
@@ -4,29 +4,32 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = {15, 5, 13, 1, 4, 18, 90, 18};
+        int[] arr = {15, 5, 13, 1, 4, 18, 90, 21};
         selectionSort(arr);
         System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 
-    public static void selectionSort(int[] arr) {
+    static void selectionSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
+            int last = n - i - 1;
+            int maxIndex = getMax(arr, 0, last);
+
+            if (maxIndex != last) {
+                swap(arr, last, maxIndex);
+                System.out.println("After pass " + (i + 1) + ": " + Arrays.toString(arr));
             }
-            if (i != minIndex) {
-                swap(arr, i, minIndex);
-            }
-            System.out.print("After pass " + (i + 1) + ": ");
-            for (int j : arr) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
         }
+    }
+
+    static int getMax(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
     }
 
     public static void swap(int[] arr, int i, int j) {
