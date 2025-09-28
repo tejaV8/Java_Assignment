5cfb6d73895acf507934d4aa33219abb773c680e
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-09-28 20:57:23 +0530

    Strings

---
+++ src/com/Strings/StringOperations.java
@@ -0,0 +1,24 @@
+package com.Strings;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        //  .equals()
+//        String a = "OP";
+//        String b = "OP";
+//
+//        System.out.println(a.equals(b));
+        //  .charAt()
+//        String a = "Optimus";
+//        System.out.println(a.charAt(3));
+
+        System.out.println(56);
+//        Integer num = new Integer(56);
+//        System.out.println(num.toString());
+//        System.out.println(num);
+//        System.out.println("Kunal");
+//        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+////
+//        String name = null;
+//        System.out.println(name);
+    }
+}
